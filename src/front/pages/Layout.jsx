c7e7b041832d9376c39ef782dac4a6a81a4a1f46
Mais02 <maryaisl02@gmail.com>
2025-09-20 04:57:50 +0000

    Navbar, Layout y Routes

--- src/front/pages/Layout.jsx
+++ src/front/pages/Layout.jsx
@@ -2,14 +2,20 @@ import { Outlet } from "react-router-dom/dist"
 import ScrollToTop from "../components/ScrollToTop"
 import { Navbar } from "../components/Navbar"
 import { Footer } from "../components/Footer"
+import {useLocation} from "react-router-dom"
 
 // Base component that maintains the navbar and footer throughout the page and the scroll to top functionality.
 export const Layout = () => {
+    const location=useLocation()
+    const isRegisterPath = location.pathname =="/registro-usuario" ? true : false
+    const isLoginPath = location.pathname == "/login" ? true : false
+    
+    /*use condicional to not show navbar y footer in other  ==*/
     return (
         <ScrollToTop>
-            <Navbar />
+            {isRegisterPath || isLoginPath ? "" : <Navbar />}
                 <Outlet />
-            <Footer />
+            {isRegisterPath || isLoginPath  ? "" : <Footer />}
         </ScrollToTop>
     )
 }
