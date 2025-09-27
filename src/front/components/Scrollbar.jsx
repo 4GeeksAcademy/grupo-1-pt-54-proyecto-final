61d5b70b77c54cb3bf2eaf4664d5113c976725e5
Songbird98 <alancabcon@gmail.com>
2025-09-27 02:06:09 +0000

    agregar barra de busqueda

---
+++ src/front/components/Scrollbar.jsx
@@ -0,0 +1,12 @@
+import SearchComponent from './SearchComponent';
+
+
+function Scrollbar() {
+    return (
+        <div className='container-fuid'>
+            <h2 className='text-center'>Reach search</h2>
+            <SearchComponent />
+        </div>
+    );
+}
+export default Scrollbar;
