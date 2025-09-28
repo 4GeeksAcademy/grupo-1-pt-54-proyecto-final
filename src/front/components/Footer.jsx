158c486afd6bd4024234b933489db779ebd474b1
Mais02 <maryaisl02@gmail.com>
2025-09-28 06:52:15 +0000

    Vista de libros individuales, Navbar especifica

--- src/front/components/Footer.jsx
+++ src/front/components/Footer.jsx
@@ -1,5 +1,5 @@
 export const Footer = () => (
-	<div className="container-fluid" style={{ padding: "50px" }}>
+	<div className="container-fluid" style={{ padding: "0 50px 0 50px" }}>
 		<footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
 			<i className="fa-solid fa-book-open" />
 			<span className="mb-3 mb-md-0 text-body-secondary">© 2025, Reading List</span>
