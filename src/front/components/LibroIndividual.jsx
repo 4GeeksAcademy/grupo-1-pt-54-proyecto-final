599e36d608ee817acf330b615f92321669283e74
Mais02 <maryaisl02@gmail.com>
2025-09-29 05:32:35 +0000

    Opcion de eliminar libro y arreglar logica de Libro Invidual

--- src/front/components/LibroIndividual.jsx
+++ src/front/components/LibroIndividual.jsx
@@ -1,25 +1,32 @@
-import { Link, useParams } from "react-router-dom";
-import useGlobalReducer from "../hooks/useGlobalReducer";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 export const LibroIndividual = () => {
-  const { store } = useGlobalReducer()
-  const { theId } = useParams()
+  const { id } = useParams()
+  const [libro, setLibro] = useState(null);
 
-  const singleLibro = store.libros.find(libro => libro.id === parseInt(theId));
+  const fetchLibro = async () => {
+    try {
+       const response = await fetch(`api/books/${id}`);
+      const data = await response.json();
+      setLibro(data);
+    } catch (error) {
+      console.error("Error fetching book details:", error);
+      setLibro(null);
+    }
+  };
 
-  if (!singleLibro) {
-    return <p>Book not found...</p>;
-  }
+  useEffect(() => fetchLibro(), [id])
   return (
     <div className="container row align-items-center" style={{ justifySelf: "center" }}>
       <div className="col-4 col-lg-5" style={{ /*padding: "82px 0 0 50px",*/ textAlign: "center" }}>
-        <img src={singleLibro?.image} alt="portada de libro" className="img-fluid" style={{ borderRadius: "8px" }} />
+        <img src={libro.image} alt="portada de libro" className="img-fluid" style={{ borderRadius: "8px" }} />
       </div>
       <div className="col-7" style={{ justifyItems: "center", fontFamily: "'M PLUS 2', sans-serif", color: "darkolivegreen" }}>
-        <h1 className="mb-3 fw-medium" style={{ marginBottom: "14px" }}>{singleLibro?.title}</h1>
-        <h6 className="mb-3 fw-medium" style={{ fontSize: "xx-large", marginBottom: "14px" }}>{singleLibro?.author}</h6>
-        {/* <span className="display-4">{singleLibro?.progreso}</span> */}
-        <h4 className="display-4 blockquote" style={{ textAlign: "justify" }}>{singleLibro?.sinopsis}</h4>
+        <h1 className="mb-3 fw-medium" style={{ marginBottom: "14px" }}>{libro.title}</h1>
+        <h6 className="mb-3 fw-medium" style={{ fontSize: "xx-large", marginBottom: "14px" }}>{libro.author}</h6>
+        {/* <span className="display-4">{libro?.progreso}</span> */}
+        <h4 className="display-4 blockquote" style={{ textAlign: "justify" }}>{libro.sinopsis || "Sin sinopsis disponible."}</h4>
       </div>
     </div>
   );
