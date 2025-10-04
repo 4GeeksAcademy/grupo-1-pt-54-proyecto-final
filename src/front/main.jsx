42d753e162a4274cc499c305a4b532d2528a1220
Isaí Berber <isai.berber@gmail.com>
2025-10-04 02:42:36 +0000

    Scrollbar por BackendURL

--- src/front/main.jsx
+++ src/front/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {Scrollbar} from './components/Scrollbar';
 import './index.css'  // Global styles for your application
 import { RouterProvider } from "react-router-dom";  // Import RouterProvider to use the router
 import { router } from "./routes";  // Import the router configuration
@@ -11,7 +10,7 @@ import { BackendURL } from './components/BackendURL';
 const Main = () => {
     
     if(! import.meta.env.VITE_BACKEND_URL ||  import.meta.env.VITE_BACKEND_URL == "") return (    
-              <Scrollbar/>
+              <BackendURL/>
         );
     return (
         <React.StrictMode>  
