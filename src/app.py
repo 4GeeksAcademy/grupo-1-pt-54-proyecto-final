c82aef46c1dc364e08b7ddca50a1a7c9055ac5b0
raaronpgomez <raaronpgomez@gmail.com>
2025-10-28 05:16:03 +0000

    Backend de Delete a Frontend

--- src/app.py
+++ src/app.py
@@ -20,7 +20,7 @@ import os
 from flask_mail import Mail, Message
 import datetime
 from sqlalchemy import select
-# from models import Person
+
 ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
 static_file_dir = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), '../dist/')
@@ -36,7 +36,6 @@ app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
 app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
 mail = Mail(app)
 app.url_map.strict_slashes = False
-# database condiguration
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
     app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
@@ -46,19 +45,14 @@ else:
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db, compare_type=True)
 db.init_app(app)
-# add the admin
 setup_admin(app)
-# add the admin
 setup_commands(app)
-# Add all endpoints form the API with a "api" prefix
 app.register_blueprint(api, url_prefix='/api')
-# Handle/serialize errors like a JSON object
 
 
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
-# generate sitemap with all your endpoints
 
 
 @app.route('/')
@@ -66,7 +60,6 @@ def sitemap():
     if ENV == "development":
         return generate_sitemap(app)
     return send_from_directory(static_file_dir, 'index.html')
-# any other endpoint will try to serve it like a static file
 
 
 @app.route('/<path:path>', methods=['GET'])
@@ -74,7 +67,7 @@ def serve_any_other_file(path):
     if not os.path.isfile(os.path.join(static_file_dir, path)):
         path = 'index.html'
     response = send_from_directory(static_file_dir, path)
-    response.cache_control.max_age = 0  # avoid cache memory
+    response.cache_control.max_age = 0 
     return response
 
 
@@ -142,8 +135,6 @@ def verify_token(token):
         print(error)
         db.session.rollback()
         return jsonify({"msg": "Token inválido"}), 400
-# forgot_password
-
 
 @app.route('/api/forgot', methods=['POST'])
 def forgot_password():
@@ -235,7 +226,7 @@ def get_single_book(id):
         "id": book.get("id"),
         "title": book.get("title", "Sin título"),
         "author": book.get("author", "Autor desconocido"),
-        "cover_i": book.get("cover_i", None),  # can be None if no image
+        "cover_i": book.get("cover_i", None),
         "progreso": book.get("progreso", 0),
         "sinopsis": book.get("sinopsis", "")
     }
